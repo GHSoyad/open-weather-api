065b950b2be4259e6fd89c223175e95883831ba0
GHSoyad <ghsoyad@gmail.com>
2022-09-06 01:23:10 +0800

    Extracted data to show on website

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 // Never put API key like this in here
 const API_KEY = '259c42f6a252ff8827dc6d6f8b0b8f5d';
 
-const loadWeatherData = async (city = 'dhaka') => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+const loadWeatherData = async (city) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        displayWeatherData(data);
     } catch (error) {
         console.log(error)
     }
 }
 
-loadWeatherData();
+loadWeatherData('dhaka');
 
-document.getElementById('search-btn').addEventListener('click', function () {
-    const searchField = document.getElementById('search-field');
-    const searchFieldCity = searchField.value;
+const displayWeatherData = (city) => {
 
-    if (searchFieldCity === '') {
-        return alert('Please Enter a city name first!!')
+    if (city.cod && city.cod == 404) {
+        return alert(`${city.message}!!`)
     }
+    console.log(city)
 
-    loadWeatherData(searchFieldCity);
-})
+    const cityName = document.getElementById('city-name');
+    cityName.innerText = `${city.name}`;
+
+    const cityTemp = document.getElementById('city-temp');
+    cityTemp.innerText = `${city.main.temp}`;
+
+    const cityWeather = document.getElementById('city-weather');
+    cityWeather.innerHTML = '';
+    const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.innerHTML = '';
+
+    city.weather.forEach(weather => {
+        const p = document.createElement('p');
+        p.classList.add('d-inline-block', 'mx-3')
+        p.innerText = `${weather.main}`;
+        cityWeather.appendChild(p)
+
+        const img = document.createElement('img')
+        img.setAttribute('src', `https://openweathermap.org/img/wn/${weather.icon}@2x.png`);
+        weatherIcon.appendChild(img);
+    });
+}
